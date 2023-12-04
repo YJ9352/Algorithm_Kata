4dfba0ebf006619ed2ef2fbd1ce58399aac490a8
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-04 15:19:28 +0900

    Add files via upload

---
+++ Kata_231204.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+fun main() {
+    class Solution {
+        fun solution(num1: Int, num2: Int): Int {
+            var answer: Int = 0
+
+            if (-50000 >= num1 && num2 <= 50000) {
+
+            }
+            return num1 - num2
+        }
+    }
+}
