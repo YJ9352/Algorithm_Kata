da5d6ed78572adee47c9ee217191d51661bb2c2a
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-09 23:12:37 +0900

    Update Kata231209.kt
    수정

--- Kata231209.kt
+++ Kata231209.kt
@@ -6,11 +6,11 @@ fun main() {
         fun solution(n: Int): Int {
             var answer: Int = 0
 
-            for (x in 2 until n-1) {
+            for (x in 2 .. n-1) {
                 if (n % x == 1) return x
             }
 
-            return n - 1
+            return n
         }
     }
 }
