232108903afdcc7892fbc8fac66f8427a3469ed6
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-11 10:39:13 +0900

    kata 16 주석 및 예제 추가

--- Kata231211.kt
+++ Kata231211.kt
@@ -1,9 +1,17 @@
 package codekata
 
+// x만큼 간격이 있는 n개의 숫자
 fun main() {
+    // 사이트 내 답안
     class Solution {
         fun solution(x: Int, n: Int) = LongArray(n){ x.toLong() * (it + 1) }
     }
+
+    // 예제로 만든 것
+    fun solution(x: Int, n: Int) = LongArray(n) { x.toLong() * (it + 1) }
+
+    val result = solution(2, 5)
+    println(result.contentToString())
 }
 
 
