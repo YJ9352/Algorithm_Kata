0b3380bd1a16e3ae89859817aaff573d867c804f
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-28 08:52:19 +0900

    kata 31

---
+++ Kata231228.kt
@@ -0,0 +1,20 @@
+package codekata
+
+fun main() {
+    // 수박수박수박수박수박수?
+    class Solution {
+        fun solution(n: Int): String {
+            var answer = ""
+
+            for (i in 0 until n) {
+                if (i % 2 == 0) {
+                    answer += "수"
+                } else {
+                    answer += "박"
+                }
+            }
+
+            return answer
+        }
+    }
+}
